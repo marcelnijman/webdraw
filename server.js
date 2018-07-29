9cbac2b88704a2db2436cdb5a0290c738f0ca67d
marcelnijman <marcel@marcelnijman.eu>
2018-07-29 15:36:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 console.log("My socket server is running");
 
