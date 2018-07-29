83f57181bdfa0c31a921d5aa1d7f4fccc707116b
marcelnijman <marcel@marcelnijman.eu>
2018-07-29 15:56:46 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 
-//app.use(express.static('public'));
-
 console.log("My socket server is running");
 
 var socket = require('socket.io');
